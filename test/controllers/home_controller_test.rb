9f8c6b6b17cbcc60fed1a6d21795eaae4777c3c5
Vladislav Manolov <vm00328@surrey.ac.uk>
2020-11-25 15:49:55 +0000

    Improved testing and added functionality to the footer

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -4,8 +4,9 @@ class HomeControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get root_url
     assert_response :success
-    assert_select 'title', 'My Website'
-    assert_select 'p', 'Welcome to my website!'
+    assert_select 'title', 'Sofia Open'
+    assert_select 'h1', 'A website for my com2025 coursework.'
+    assert_select 'h2', 'Sofia Open Tennis Tournament'
+    assert_select 'h3', 'Welcome to my website!'
   end
-
 end

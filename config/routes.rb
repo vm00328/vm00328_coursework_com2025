887548ec662ba6937bc3457488e2814ae682020a
Vladislav Manolov <vm00328@surrey.ac.uk>
2020-12-03 17:20:31 +0000

    Created a new scaffold that is a shopping cart

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :carts
   resources :racquets
   resources :clothings
   #Resource paths
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_03_143351) do
+ActiveRecord::Schema.define(version: 2020_12_03_171548) do
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "clothings", force: :cascade do |t|
     t.string "gender"

12b6dba882d2d9cb821747ff852dd3c91b0dbb1c
Vladislav Manolov <vm00328@surrey.ac.uk>
2020-12-08 13:39:54 +0000

    Added more tests for the cords

--- test/models/cord_test.rb
+++ test/models/cord_test.rb
@@ -5,13 +5,36 @@ class CordTest < ActiveSupport::TestCase
     @cord = cords(:one)
 end
 
+  test 'should save valid cord' do
+    cord = Cord.new
+    cord.name = 'My Cord'
+    cord.colour = 'orange'
+    cord.racquet = @racquet
+    cord.save
+    refute cord.valid?
+  end
+
   test 'should not save empty cord' do
     cord = Cord.new
     cord.save
     refute cord.valid?
   end
 
-  test 'should save valid cord' do
+  test 'should not save cord if name is not provided' do
+    cord = Cord.new
+    cord.racquet = @racquet
+    cord.save
+    refute cord.valid?
+  end
+
+  test 'should not save cord if racquet is not provided' do
+    cord = Cord.new
+    cord.name = 'My Cord'
+    cord.save
+    refute cord.valid?
+  end
+
+  test 'should not save cord if colour is not provided' do
     cord = Cord.new
     cord.name = 'My Cord'
     cord.racquet = @racquet
